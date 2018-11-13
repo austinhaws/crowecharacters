356b0357fe2ae1107541dbb776b8f4542676433a
Austin Haws <ahaws@utah.gov>
2018-11-12 20:26:42 -0700

    Hide Doll Edit category if no images to show in it
    Girl has no cloaks

--- client/src/Common/Components/ImageList/ByCategory/ImageListByCategory.js
+++ client/src/Common/Components/ImageList/ByCategory/ImageListByCategory.js
@@ -57,18 +57,22 @@ class ImageListByCategory extends React.Component {
 		return (
 			<div className="images-by-category-list">
 				{this.props.globalData.imageCategories ?
-					this.props.globalData.imageCategories.map(category => (
-						<Category
-							key={category.guid}
-							categoryGuid={category.guid}
-							imagesInCategory={this.props.images.filter(image => image.image_category_guid === category.guid)}
-							selectedImageGuids={this.props.selectedImageGuids}
-							onCategorySelect={this.categorySelect}
-							isSelected={category.guid === this.state.selectedCategoryGuid}
-							onImageAdd={this.props.onImageAdd}
-							onImageRemove={this.props.onImageRemove}
-						/>
-					))
+					this.props.globalData.imageCategories.map(category => {
+						const imagesInCategory = this.props.images.filter(image => image.image_category_guid === category.guid);
+						return (
+							imagesInCategory.length === 0 ? <React.Fragment/> : (
+							<Category
+								key={category.guid}
+								categoryGuid={category.guid}
+								imagesInCategory={imagesInCategory}
+								selectedImageGuids={this.props.selectedImageGuids}
+								onCategorySelect={this.categorySelect}
+								isSelected={category.guid === this.state.selectedCategoryGuid}
+								onImageAdd={this.props.onImageAdd}
+								onImageRemove={this.props.onImageRemove}
+							/>
+						));
+					})
 					: undefined
 				}
 			</div>
